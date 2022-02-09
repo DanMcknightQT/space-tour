f5379e298a67c7b57746f52ec1559ecafd1dbaa0
Aaron Alderdyce <aaron_alderdyce@hotmail.com>
2022-02-09 14:59:53 -0800

    correctly linked the routing system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HomeComponent } from './components/home/home.component';
-import { SubHeadingBarComponent } from './components/sub-heading-bar/sub-heading-bar.component';
-import { PlanetNameComponent } from './components/planet-name/planet-name.component';
-import { PlanetInfoComponent } from './components/planet-info/planet-info.component';
 import { DestinationComponent } from './components/destination/destination.component';
 import { CrewComponent } from './components/crew/crew.component';
 import { TechnologyComponent } from './components/technology/technology.component';
@@ -17,12 +12,7 @@ import { NoPageComponent } from './components/no-page/no-page.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    SidebarComponent,
     HomeComponent,
-    SubHeadingBarComponent,
-    PlanetNameComponent,
-    PlanetInfoComponent,
     DestinationComponent,
     CrewComponent,
     TechnologyComponent,
