8b35b5cf5f569de70732583a9413d38979c316b7
Your Name <you@example.com>
2022-02-09 22:36:12 -0500

    path match added to "*" redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   {path:"", redirectTo:"/home", pathMatch:"full"},
   {path:"home", component:HomeComponent},
   {path:"404", component:NotFoundComponent},
-  {path:"*", redirectTo:"/404"}
+  {path:"*", redirectTo:"/404", pathMatch:"full"}
 
 ];
 
