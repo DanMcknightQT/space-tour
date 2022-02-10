b849123087174007082a5e31e9e235777509e35a
Your Name <you@example.com>
2022-02-09 20:31:53 -0500

    routes to home & 404 , and redirection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"/home", pathMatch:"full"},
+  {path:"home", component:HomeComponent},
+  {path:"404", component: NotFoundComponent},
+  {path:"*", redirectTo:"/404"}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
