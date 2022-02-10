91ea8c11720d186caf38861d3a54f205f43e0d26
Your Name <you@example.com>
2022-02-09 22:55:14 -0500

    adding routes to main components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { CrewComponent } from './components/crew/crew.component';
+import { DestinationComponent } from './components/destination/destination.component';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { TechnologyComponent } from './components/technology/technology.component';
 
 const routes: Routes = [
   {path:"", redirectTo:"/home", pathMatch:"full"},
   {path:"home", component:HomeComponent},
+  {path:"destination", component:DestinationComponent},
+  {path:"crew",component:CrewComponent},
+  {path:"technology", component: TechnologyComponent},
   {path:"404", component:NotFoundComponent},
   {path:"*", redirectTo:"/404", pathMatch:"full"}
 
