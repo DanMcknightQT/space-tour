0efeeff51c98ec697b0a8942be54e21f2d44367c
Aaron Alderdyce <aaron_alderdyce@hotmail.com>
2022-02-10 03:20:12 -0800

    Very proud. Found  bug and fixed myself

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../app/app.component.css']
+  styleUrls: ['./app.component.html']
 })
 export class AppComponent {
   title = 'space-tour';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { DestinationComponent } from './components/destination/destination.component';
@@ -9,7 +10,6 @@ import { CrewComponent } from './components/crew/crew.component';
 import { TechnologyComponent } from './components/technology/technology.component';
 import { NoPageComponent } from './components/no-page/no-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -25,7 +25,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
 
   ],
   providers: [],
