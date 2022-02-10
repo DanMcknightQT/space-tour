2da6cb2bf7660f860abcb46fcc346f5a28561be6
Aaron Alderdyce <aaron_alderdyce@hotmail.com>
2022-02-09 19:23:30 -0800

    banging head against wall, but got answer

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: []
+  styles: ["src/styles.css"]
 })
 export class NavbarComponent implements OnInit {
 
