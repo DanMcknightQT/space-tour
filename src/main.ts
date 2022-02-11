03e5a6bea9357e0080d7f395a25999d999ad7298
PatrickM-NAVDNA <PatrickM@avdna.com>
2022-02-11 11:01:14 -0600

    setting up export vars

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as JSONdata from './app/data.json'
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -10,3 +11,15 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+let data: any = {...JSONdata};
+
+data = Object.entries(data);
+
+data = data[3][1];
+//console.log(data)
+
+export let crewData = data.crew;
+export let destinationsData = data.destinations;
+export let techData = data.technology;
