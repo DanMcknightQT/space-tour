33d9c8b5337e5b2e106c4f7f18463e4462b5cc68
Aaron Alderdyce <aaron_alderdyce@hotmail.com>
2022-02-12 15:29:44 -0800

    putting stuff together.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.html']
+  styleUrls: ['./app.component.html',
+              '../assets/shared/logo.svg'
+            ]
 })
 export class AppComponent {
   title = 'space-tour';
