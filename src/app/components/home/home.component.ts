bad919b9918dbffe617282f74d49df026d296747
Aaron Alderdyce <aaron_alderdyce@hotmail.com>
2022-02-13 11:33:23 -0800

    trying to figure out this problem.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,10 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: 
-    ['./home.component.css',
-    '../../app.component.css'
-    ],
+  styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent implements OnInit {
