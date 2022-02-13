54b86a4dd8f3697ed902491c3c3c8749fd4d48c8
Aaron Alderdyce <aaron_alderdyce@hotmail.com>
2022-02-12 16:45:21 -0800

    pushing up some changes.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoService } from 'src/app/services/info.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: 
-  ['./home.component.css',
-  '../../app.component.css'
-]
+    ['./home.component.css',
+    '../../app.component.css'
+    ],
+  providers: [InfoService]
 })
+
+
 
 export class HomeComponent implements OnInit {
 
